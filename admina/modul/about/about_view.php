e9c24061afc2fce2e9fa111c3e6973699b604065
wildantea <wildannudin@gmail.com>
2016-03-04 13:26:33 +0700

    change db

---
+++ admina/modul/about/about_view.php
@@ -0,0 +1,35 @@
+
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        About
+                    </h1>
+                      
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-xs-12">
+                        <div class="box box-primary">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Feeder Importer</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                 
+                    Feeder Importer adalah tool gratis untuk import data ke feeder dikti. 
+                  </p>
+                  Kami juga bisa membantu jika sistem akademik mau di integrasikan ke feeder. Kami telah mengerjakan feeder integrator UIN Bandung untuk mempermudah pengambilan data dari Sistem akademik untuk kemudian di teruskan ke feeder. 
+                  <p>Jika Punya pertanyaan ataupun saran silakan hubungi saya</p>
+                  Email : wildannudin@gmail.com<br>
+                  HP : 085720081081<br>
+                  Blog : wildantea.com<br>
+
+                  Terimakasih telah mencoba feeder impoter ini. 
+
+                </div><!-- /.box-body -->
+              </div>
+                        </div>
+                    </div>
+       
+                </section><!-- /.content -->
