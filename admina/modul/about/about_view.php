3c077723a4500361bfa961a4b223fa609d6e9c29
wildantea <wildannudin@gmail.com>
2016-04-19 11:24:14 +0700

    update 1.0.4

--- admina/modul/about/about_view.php
+++ admina/modul/about/about_view.php
@@ -23,8 +23,8 @@
                 1. Supaya process execution lebih dari 30 detik atau jika mengalami error <span style="color:#f00">Maximum execution time of 30 seconds exceeded</span> Maka langkah yang harus dilakukan adalah merubah file php.ini yang ada di directory c:/xampp/php/php.ini. cari <span style="color:#f00">max_execution_time=30</span> kemudian ubah 30 jadi 3600 (berarti anda merubah process menjadi 3000 detik atau 1 jam)
                 <br>
                 2. Jika mengalami error <span style="color:#f00">PHP: Fatal Error: Allowed Memory Size</span> , Maka yang harus dilakukan sama, rubah php.ini kemudian cari <span style="color:#f00">memory_limit=</span> tinggal di perbesar memory misal jadi <span style="color:#f00">memory_limit=128M</span> (berarti anda set memory 120 megabyte)
-
-
+                <br>
+                Jangan Lupa Restart Apache
                 <p></p>
                  
                     
@@ -38,6 +38,13 @@
                   Blog : wildantea.com<br>
 
                   Terimakasih telah mencoba feeder impoter ini. 
+
+                  <p>
+                    Jika kawan - kawan mau menyumbang saya ga nolak :D
+                    a/n : mohamad wildannudin
+                    norek : 7740376386
+                    rek : BCA
+                  </p>
 
                 </div><!-- /.box-body -->
               </div>
